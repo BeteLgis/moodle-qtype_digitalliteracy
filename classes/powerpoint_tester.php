6f6a5985ea14156acec6f8b332c3d46e2a19c589
Fyodor Deryabin <fvderyabin@edu.hse.ru>
2020-07-09 11:29:40 +0500

    Add trial binary marking

--- classes/powerpoint_tester.php
+++ classes/powerpoint_tester.php
@@ -13,25 +13,26 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
         // TODO: Implement validate() method.
     }
 
-    public function compareFiles($source, $sample)
+    public function compareFiles($source, $sample, $isBinary)
     {
         // TODO validation!
         $samplePptx = IOFactory::load($source);
         $analysPptx = IOFactory::load($sample);
 
         $cmpSlides = $this->compareSlidesCount($samplePptx, $analysPptx);
-        $analysText = $this->testFontsText($samplePptx, $analysPptx);
+        $analysText = $this->testFontsText($samplePptx, $analysPptx, $isBinary);
         $cmpShapes = $this->compareShapes($samplePptx, $analysPptx);
         $cmpLayouts = $this->compareLayout($samplePptx, $analysPptx);
-        $cmpParagraphs = $this->compareParagraphsBullets($samplePptx, $analysPptx);
+
+        $cmpParagraphs = $this->compareParagraphsBullets($samplePptx, $analysPptx, $isBinary);
         $scored = 0;
         $max = 0;
         if(system('python --version')){
             system('python powerpoint_tester_shapes.py '.$source.' '.$sample, $scored);
             system('python powerpoint_tester_shapes.py '.$source.' '.$sample, $max);
         }
-        $scored += $cmpShapes[0] + $cmpSlides[0] + $analysText[0] + $cmpLayouts[0] + $cmpParagraphs[0];
-        $max += $cmpShapes[1] + $cmpSlides[1] + $analysText[1] + $cmpLayouts[1] + $cmpParagraphs[1];
+        $scored += $cmpShapes + $cmpSlides + $analysText[0] + $cmpLayouts[0] + $cmpParagraphs[0];
+        $max +=  2 + $analysText[1] + $cmpLayouts[1] + $cmpParagraphs[1];
         if ($max == 0)
             $max = 1;
         return $scored/$max;
@@ -72,10 +73,12 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
      * Function to get array of int in (min,max) format
      * @params: $sample_pptx -- sample presentation
      * $tested_pptx -- tested presentation
-     * @return -- array<int,int>, where first element is minimum count of slides, second element -- maximum
+     * @return -- int
      */
     private function compareSlidesCount($sample_pptx , $tested_pptx){
-        return array(min(count($tested_pptx->getAllSlides()),count($sample_pptx->getAllSlides())),max(count($tested_pptx->getAllSlides()),count($sample_pptx->getAllSlides())));
+        if(count($tested_pptx->getAllSlides()) == count($sample_pptx->getAllSlides()))
+            return 1;
+        return 0;
     }
 
     //Получение текста с презентации
@@ -127,7 +130,10 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
      * @return mark from 0 to 1 with comparing.
      */
     private function compareShapes($sample_pptx, $tested_pptx){
-        return array(min($this->countNotRichTextShapes($sample_pptx), $this->countNotRichTextShapes($tested_pptx)),max($this->countNotRichTextShapes($tested_pptx),$this->countNotRichTextShapes($sample_pptx)));
+        if($this->countNotRichTextShapes($sample_pptx) == $this->countNotRichTextShapes($tested_pptx)){
+            return 1;
+        }
+        return 0;
     }
 
     /*
@@ -153,15 +159,23 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
      * @params: $testing -- testing pptx
      * @return -- mark from 0 to 1
      */
-    public function testFontsText($sample_pptx, $tested_pptx){
+    public function testFontsText($sample_pptx, $tested_pptx, $isBinary){
         $smplSlides = $this->getSlidesArray($sample_pptx);
         $tstSlides = $this->getSlidesArray($tested_pptx);
         $max = 0;
         $scored = 0;
+
         for ($i = 0; $i < min(count($smplSlides),count($tstSlides)); $i++){
             $temp = $this->analysTextFromSlide($smplSlides[$i], $tstSlides[$i]);
-            $scored += $temp[0];
-            $max += $temp[1];
+            if($isBinary){
+                if($temp[0] == $temp[1])
+                    $scored+=1;
+                $max += 1;
+            }
+            else{
+                $scored += $temp[0];
+                $max += $temp[1];
+            }
         }
         return array($scored,$max);
     }
@@ -260,7 +274,7 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
         return 0;
     }
 
-    private function compareParagraphs($par1, $par2){
+    private function compareParagraphs($par1, $par2, $isBinary){
         $sample = $par1->getBulletStyle();
         $test = $par2->getBulletStyle();
         if($sample == null || $test == null){
@@ -275,10 +289,19 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
             $scored += 1;
         if($sample->getBulletNumericStartAt() == $test->getBulletNumericStartAt() )
             $scored += 1;
+
+        
+        if($isBinary){
+            if($scored == 4)
+                return array(1, 1);
+            else
+                return array(0,1);
+        }
+
         return array($scored, 4);
     }
 
-    private function compareParagraphsBullets($samplePptx, $analysPptx){
+    private function compareParagraphsBullets($samplePptx, $analysPptx, $isBinary){
         $sl1 = $this->getSlidesArray($samplePptx);
         $sl2 = $this->getSlidesArray($analysPptx);
         $scored = 0;
@@ -303,7 +326,7 @@ class qtype_digitalliteracy_powerpoint_tester implements qtype_digitalliteracy_c
                 }
             }
             for ($j = 0; $j < min(count($par1),count($par2));$j++){
-                $tmp = $this->compareParagraphs($par1[$j], $par2[$j]);
+                $tmp = $this->compareParagraphs($par1[$j], $par2[$j], $isBinary);
                 $scored += $tmp[0];
                 $max += $tmp[1];
             }
