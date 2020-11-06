04bdcb3a18bbada8696bf879a9aa2a11e732ad8b
Айнур Шарипов <aasharipov@edu.hse.ru>
2020-11-06 17:46:05 +0300

    Version upgrade

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # DIGITAL LITERACY
-Version 0.9.0 August 2020
+Version 0.9.2 November 2020
 
-Authors: Ainur Sharipov and Fedor Deryabin, Higher School of Economics, Russia
+Authors: Ainur Sharipov, Higher School of Economics, Russia
 # INSTALLATION
 DigitalLiteracy requires two separate plug-ins: one for the question type and one for the special interactive behaviour. 
 
--- version.php
+++ version.php
@@ -2,12 +2,12 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020041473;
+$plugin->version   = 2020041475;
 $plugin->requires  = 2018050800;
 $plugin->cron = 0;
 $plugin->component = 'qtype_digitalliteracy';
 $plugin->maturity = MATURITY_BETA;
-$plugin->release = '0.9.0';
+$plugin->release = '0.9.2';
 
 $plugin->dependencies = array(
     'qbehaviour_interactive_for_digitalliteracy' => 2020041451
